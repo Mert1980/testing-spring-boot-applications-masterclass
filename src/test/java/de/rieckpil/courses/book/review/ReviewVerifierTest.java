0f00fb270bd16c83f670d32ddcc19a2e571aef4b
mdmirok <mert.demirok@vdab.be>
2023-11-16 08:41:33 +0100

    use ParameterizedTest to read test params out of a CSV file

--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -1,9 +1,12 @@
 package de.rieckpil.courses.book.review;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class ReviewVerifierTest {
 
@@ -11,14 +14,29 @@ class ReviewVerifierTest {
 
   @BeforeEach
   public void beforeEach(){
-    System.out.println("Before each");
     reviewVerifier = new ReviewVerifier();
   }
   @Test
   void shouldFailWhenUseSwearWords() {
     String review = "I don't like this shit";
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
-    Assert.assertFalse(result);
+    assertFalse(result, "ReviewVerifier did not detect swear word");
   }
 
+  @Test
+  @DisplayName("Should fail when review contains 'lorem ipsum'")
+  void testLoremIpsum(){
+    String review = """
+      Lorem ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
+      """;
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect lorem ipsum");
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/badReview.csv")
+  void shouldFailWhenReviewIsOfBadQuality(String review) {
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect bad review");
+  }
 }
