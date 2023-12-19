1fc55938a97d912f6e605be9bc5aaf0f4bfe119d
mdmirok <mert.demirok@vdab.be>
2023-12-19 07:46:21 +0100

    pre-populate test data with a SQL script

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
@@ -1,32 +1,21 @@
 package de.rieckpil.courses.book.review;
 
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
-// @Testcontainers(disabledWithoutDocker = true)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ReviewRepositoryNoInMemoryTest {
 
-  // @Container
   static PostgreSQLContainer<?> container = (PostgreSQLContainer) new PostgreSQLContainer<>("postgres:12.3")
     .withDatabaseName("test")
     .withUsername("duke")
@@ -45,16 +34,7 @@ class ReviewRepositoryNoInMemoryTest {
   }
 
   @Autowired
-  private EntityManager entityManager;
-
-  @Autowired
   private ReviewRepository cut;
-
-  @Autowired
-  private DataSource dataSource;
-
-  @Autowired
-  private TestEntityManager testEntityManager;
 
   @BeforeEach
   void beforeEach() {
@@ -62,32 +42,8 @@ class ReviewRepositoryNoInMemoryTest {
   }
 
   @Test
-  void notNull() throws SQLException {
-    assertNotNull(entityManager);
-    assertNotNull(cut);
-    assertNotNull(dataSource);
-    assertNotNull(testEntityManager);
-
-    System.out.println(dataSource.getConnection().getMetaData().getDatabaseProductName());
-
-    Review review = new Review();
-    review.setContent("Duke");
-    review.setTitle("Review 01");
-    review.setCreatedAt(LocalDateTime.now());
-    review.setRating(5);
-    review.setBook(null);
-    review.setUser(null);
-
-    Review result = cut.save(review);
-    assertNotNull(result.getId());
-  }
-
-  //@Test
   @Sql(scripts = "/scripts/INIT_REVIEW_EACH_BOOK.sql")
   void shouldGetTwoReviewStatisticsWhenDatabaseContainsTwoBooksWithReview() {
-  }
 
-  // @Test
-  void databaseShouldBeEmpty() {
   }
 }
