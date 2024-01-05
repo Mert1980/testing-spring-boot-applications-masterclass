8a9ad795e64b00d0db2c6241364b258165b1e34a
mdmirok <mert.demirok@vdab.be>
2024-01-05 15:10:37 +0100

    more examples for the usage of MockMvc in Controller with JSONAssert and String Template

--- src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
@@ -1,8 +1,11 @@
 package de.rieckpil.courses.book.management;
 
+import java.util.List;
+
 import de.rieckpil.courses.config.WebSecurityConfig;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,11 +14,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -38,11 +38,11 @@ class BookControllerTest {
   @Test
   void shouldGetEmptyArrayWhenNoBooksExists() throws Exception {
     MvcResult mvcResult = this.mockMvc
-      .perform(MockMvcRequestBuilders.get("/api/books")
+      .perform(get("/api/books")
         .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
       .andExpect(MockMvcResultMatchers.status().is(200))
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-      .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(0)))
+      .andExpect(jsonPath("$.size()", Matchers.is(0)))
       .andDo(MockMvcResultHandlers.print())
       .andReturn();
     assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo("[]");
@@ -51,10 +51,62 @@ class BookControllerTest {
 
   @Test
   void shouldNotReturnXML() throws Exception {
+    this.mockMvc
+      .perform(get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML))
+      .andExpect(status().isNotAcceptable());
   }
 
   @Test
   void shouldGetBooksWhenServiceReturnsBooks() throws Exception {
+    Book bookOne =
+      createBook(
+        1L,
+        "42",
+        "Java 14",
+        "Mike",
+        "Good book",
+        "Software Engineering",
+        200L,
+        "Oracle",
+        "ftp://localhost:42");
+
+    Book bookTwo =
+      createBook(
+        2L,
+        "84",
+        "Java 15",
+        "Duke",
+        "Good book",
+        "Software Engineering",
+        200L,
+        "Oracle",
+        "ftp://localhost:42");
+
+    when(bookManagementService.getAllBooks()).thenReturn(List.of(bookOne, bookTwo));
+
+//    this.mockMvc
+//      .perform(get("/api/books").header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
+//      .andExpect(status().is(200))
+//      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//      .andExpect(jsonPath("$.size()", is(2)))
+//      .andExpect(jsonPath("$[0].isbn", is("42")))
+//      .andExpect(jsonPath("$[0].id").doesNotExist())
+//      .andExpect(jsonPath("$[0].title", is("Java 14")))
+//      .andExpect(jsonPath("$[1].isbn", is("84")))
+//      .andExpect(jsonPath("$[1].id").doesNotExist())
+//      .andExpect(jsonPath("$[1].title", is("Java 15")));
+
+    MvcResult mvcResult = this.mockMvc
+      .perform(get("/api/books").header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
+      .andReturn();
+
+    System.out.println(mvcResult.getResponse().getContentAsString());
+
+    String expected = """
+[{"title":"Java 14","isbn":"42","author":"Mike","genre":"Software Engineering","thumbnailUrl":"ftp://localhost:42","description":"Good book","publisher":"Oracle","pages":200},{"title":"Java 15","isbn":"84","author":"Duke","genre":"Software Engineering","thumbnailUrl":"ftp://localhost:42","description":"Good book","publisher":"Oracle","pages":200}]""";
+
+    JSONAssert.assertEquals(expected, mvcResult.getResponse().getContentAsString(), true);
   }
 
   private Book createBook(Long id, String isbn, String title, String author, String description, String genre, Long pages, String publisher, String thumbnailUrl) {
