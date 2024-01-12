59af0b25f481483383cf36e8a4ddf8908fcd685d
mdmirok <mert.demirok@vdab.be>
2024-01-12 16:55:09 +0100

    test Api Client with MockRestServiceServer

--- src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
+++ src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.client.HttpServerErrorException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,10 +29,34 @@ class OpenLibraryRestTemplateApiClientTest {
 
   @Test
   void shouldInjectBeans() {
+    assertNotNull(cut);
+    assertNotNull(mockRestServiceServer);
   }
 
   @Test
   void shouldReturnBookWhenResultIsSuccess() {
+
+    this.mockRestServiceServer
+      .expect(MockRestRequestMatchers.requestTo(Matchers.containsString(ISBN)))
+        //.expect(MockRestRequestMatchers.requestTo("/api/books?jscmd=data&format=json&bibkeys=" + ISBN))
+        .andRespond(
+            withSuccess(
+                new ClassPathResource("/stubs/openlibrary/success-" + ISBN + ".json"),
+                MediaType.APPLICATION_JSON));
+
+    Book result = cut.fetchMetadataForBook(ISBN);
+
+    assertEquals("9780596004651", result.getIsbn());
+    assertEquals("Head first Java", result.getTitle());
+    assertEquals("https://covers.openlibrary.org/b/id/388761-S.jpg", result.getThumbnailUrl());
+    assertEquals("Kathy Sierra", result.getAuthor());
+    assertEquals(
+        "Your brain on Java--a learner's guide--Cover.Includes index.", result.getDescription());
+    assertEquals("Java (Computer program language)", result.getGenre());
+    assertEquals("O'Reilly", result.getPublisher());
+    assertEquals(619, result.getPages());
+
+    assertNull(result.getId());
   }
 
   @Test
