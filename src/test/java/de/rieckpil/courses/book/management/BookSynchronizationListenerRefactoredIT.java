939b44cc13c8eed9e29bd018422c379fe9877816
mdmirok <mert.demirok@vdab.be>
2024-01-25 08:32:55 +0100

    extract integration test to a central place

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerRefactoredIT.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerRefactoredIT.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -17,17 +19,82 @@ import static org.awaitility.Awaitility.given;
 
 class BookSynchronizationListenerRefactoredIT extends AbstractIntegrationTest {
 
+  private static final String ISBN = "9780596004651";
+  private static String VALID_RESPONSE;
+
+  static {
+    try {
+      VALID_RESPONSE =
+        new String(
+          BookSynchronizationListenerIT.class
+            .getClassLoader()
+            .getResourceAsStream("stubs/openlibrary/success-" + ISBN + ".json")
+            .readAllBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
   @Autowired
   private WebTestClient webTestClient;
 
   @Autowired
   private BookRepository bookRepository;
 
+  @Autowired private SqsAsyncClient sqsClient;
+
   @Test
   void shouldGetSuccessWhenClientIsAuthenticated() throws JOSEException {
+    this.webTestClient
+      .get()
+      .uri("/api/books/reviews/statistics")
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + getSignedJWT())
+      .exchange()
+      .expectStatus()
+      .is2xxSuccessful();
   }
 
   @Test
   void shouldReturnBookFromAPIWhenApplicationConsumesNewSyncRequest() {
+    this.webTestClient
+      .get()
+      .uri("/api/books")
+      .exchange()
+      .expectStatus()
+      .isOk()
+      .expectBody()
+      .jsonPath("$.size()")
+      .isEqualTo(0);
+
+    this.openLibraryStubs.stubForSuccessfulBookResponse(ISBN, VALID_RESPONSE);
+
+    this.sqsClient.sendMessage(
+      SendMessageRequest.builder()
+        .queueUrl(this.sqsClient.getQueueUrl(r -> r.queueName(QUEUE_NAME)).join().queueUrl())
+        .messageBody(
+          """
+    {
+      "isbn": "%s"
+    }
+  """.formatted(ISBN))
+        .build());
+
+    given()
+      .atMost(Duration.ofSeconds(5))
+      .await()
+      .untilAsserted(
+        () -> {
+          this.webTestClient
+            .get()
+            .uri("/api/books")
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody()
+            .jsonPath("$.size()")
+            .isEqualTo(1)
+            .jsonPath("$[0].isbn")
+            .isEqualTo(ISBN);
+        });
   }
 }
